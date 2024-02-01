6d3f0c17c9dea61fd50da24c1051e9cc1bd3644c
Beckyowino <milicentrebecca@gmail.com>
2024-02-01 15:48:59 +0300

    Added files

--- Backend_Inv_Mgt/Inventory_Management_System/urls.py
+++ Backend_Inv_Mgt/Inventory_Management_System/urls.py
@@ -14,10 +14,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+"""from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('inventory.urls'))
-]
+]"""
+
+# IMS/urls.py
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.auth import views as auth
+from django.urls import include, path
+
+urlpatterns = [
+    path("/", include("inventory.urls")),
+    path("admin/", admin.site.urls),
+    path(
+        "", auth.LoginView.as_view(template_name="inventory/login.html"), name="login"
+    ),
+    path(
+        "logout/",
+        auth.LogoutView.as_view(template_name="inventory/logout.html"),
+        name="logout",
+    ),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
